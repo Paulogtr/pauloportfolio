7ea4ef4b388f4725566537d66d85136026fb0ba7
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-07-26 14:46:23 -0300

    finalização do menu mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
 /* captura dos elementos html*/
 const subtitulo = document.querySelector('#write-animation');
+const btnMenu = document.querySelector('.btn-menu');
+const containerMobileMenu = document.querySelector('.mobile-menu')
 
 /* animação do elemento write-animation*/
 function tipoWrite(elemento){
     const writeArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     writeArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 80 * i)
+        setTimeout(() => elemento.innerHTML += letra, 100 * i)
     });
 }
 
  tipoWrite(subtitulo);
 
+//ação de clique do botão de display do menu mobile
+btnMenu.addEventListener('click', function(){
+    if(containerMobileMenu.style.display === 'none'){
+        containerMobileMenu.style.display = 'block';
+    }else{
+        containerMobileMenu.style.display = 'none';
+    }
+})
+
+
+
+//Alerta de desenvolvimento
+function faseDesenvolvimento(){setTimeout(() => alert('Portfolio em fase de desenvolvimeno'), 5000)}
+
+faseDesenvolvimento()
 
